45f70448c480db8577de2b5b37f61a9a97c3a9fe
chuyoonkong <ericchu1997@live.com>
2018-11-20 23:17:45 +0000

    Added 4 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MovieComponent } from './movie/movie.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { MovieCreateComponent } from './movie-create/movie-create.component';
+import { MovieEditComponent } from './movie-edit/movie-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieComponent,
+    MovieDetailComponent,
+    MovieCreateComponent,
+    MovieEditComponent
   ],
   imports: [
     BrowserModule
